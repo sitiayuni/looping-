c455a3d40bc61705f1d683fcdcc6c54a917a0e37
Siti Ayuni <92244072+sitiayuni@users.noreply.github.com>
2021-12-10 09:40:57 +0700

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, b1 = 0, b2 = 1, bn= 0;
+
+    cout << "Masukan Batas Deret Bilangan Fibonacci :  ";
+    cin >> n;
+	cout<<endl;
+	
+    cout << "		Hasil Deret Fibonacci  ";
+    cout <<endl;
+    cout << "------------------------------------------------";
+	cout <<endl;
+    for (int i = 1; i <= n; i++) {
+    	
+        // Mencetak dua deret fibonacci pertama.
+        
+        if(i == 1)
+        {
+            cout << " " << b1<<" ";
+            continue;
+        }
+        	if(i == 2)
+        {
+            cout << b2 << " ";
+            continue;
+        }
+        
+        bn = b1 + b2;
+        b1 = b2;
+        b2 = bn;
+         // Mencetak deret bilangan fibonacci berikutnya.
+         
+        cout << bn << " ";
+}
+    
+    return 0;
+}
