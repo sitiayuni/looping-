d5491ec888091a4817d9cfd7231857f73f6c5b92
Siti Ayuni <92244072+sitiayuni@users.noreply.github.com>
2021-12-14 23:06:23 +0700

    Update looping 2.cpp

--- looping 2.cpp
+++ looping 2.cpp
@@ -4,16 +4,20 @@ using namespace std;
 
 int main(){
 
-//deklarasi variabel	
+//deklarasi variabel
+	
 	int x, y;
 	x = 5 ;
 	y = 5 ;
 	
 //looping membentuk pola persegi
+	
 	for (int z=1; z<=x; z++){
 		for(int a=1; a<y ; a++){
 			cout<< " # ";
 		}
 		cout <<" # "<< endl;
 	}
+	
+	return 0;
 }
